5cf17102432058fdbe1f8265bdaaf1e1c6524c17
Csaba Todor <todorcs@gmail.com>
2017-01-06 22:36:42 +0000

    Created Results Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { QuizComponent } from './components/quiz/quiz.component';
+import { ResultsComponent } from './components/results/results.component';
 import { CounterComponent } from './components/quiz/counter/counter.component';
 import { QuestionsAnswersComponent } from './components/quiz/questions-answers/questions-answers.component';
 // Routing
@@ -16,6 +17,7 @@ import { AppRoutingModule, appRouting, appRoutingProviders } from './app-routing
     AppComponent,
     HomeComponent,
     QuizComponent,
+    ResultsComponent,
     CounterComponent,
     QuestionsAnswersComponent
   ],
