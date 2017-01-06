7dc8f98dfe5769892750e274b71f67e798baa560
Csaba Todor <todorcs@gmail.com>
2017-01-06 22:31:22 +0000

    Created Counter Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { QuizComponent } from './components/quiz/quiz.component';
+import { CounterComponent } from './components/quiz/counter/counter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     QuizComponent,
+    CounterComponent,
   ],
   imports: [
     BrowserModule,
