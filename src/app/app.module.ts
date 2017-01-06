d8a71fa61c23bcff15ab5fb4e677a05e345847ab
Csaba Todor <todorcs@gmail.com>
2017-01-06 22:30:51 +0000

    Created Quiz Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { QuizComponent } from './components/quiz/quiz.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    QuizComponent,
   ],
   imports: [
     BrowserModule,
