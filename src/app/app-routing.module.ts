facbbcea1442cbbde59bf08330cfe8b292210b6e
Csaba Todor <todorcs@gmail.com>
2017-01-07 22:42:03 +0000

    Added Results Component to appRoutes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component';
 import { QuizComponent } from './components/quiz/quiz.component';
+import { ResultsComponent } from './components/results/results.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'quiz', component: QuizComponent }
+  { path: 'quiz', component: QuizComponent },
+  { path: 'results', component: ResultsComponent }
 ]
 
 @NgModule({
