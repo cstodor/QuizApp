624c4374fb9e7ca50a653566979ede4c624c6ee2
Csaba Todor <todorcs@gmail.com>
2017-01-07 21:34:00 +0000

    Added Countdown from 5 minutes

--- src/app/components/quiz/counter/counter.component.ts
+++ src/app/components/quiz/counter/counter.component.ts
@@ -10,6 +10,32 @@ export class CounterComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    function countdown(elem, min, sec) {
+      var element, endTime, hours, mins, msLeft, time;
+
+      function twoDigits(n) {
+        return (n <= 9 ? "0" + n : n);
+      }
+
+      function updateTimer() {
+        msLeft = endTime - (+new Date);
+        if (msLeft < 1000) {
+          element.innerHTML = "Time is up!";
+        } else {
+          time = new Date(msLeft);
+          hours = time.getUTCHours();
+          mins = time.getUTCMinutes();
+          element.innerHTML = (hours ? hours + ':' + twoDigits(mins) : mins) + ':' + twoDigits(time.getUTCSeconds());
+          setTimeout(updateTimer, time.getUTCMilliseconds() + 500);
+        }
+      }
+
+      element = document.getElementById(elem);
+      endTime = (+new Date) + 1000 * (60 * min + sec) + 500;
+      updateTimer();
+    }
+
+    countdown("counter", 5, 0);
   }
 
 }
