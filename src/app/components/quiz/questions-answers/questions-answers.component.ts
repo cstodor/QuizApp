a2dcb54cc2256f213807b710d5f08ce6f19b825f
Csaba Todor <todorcs@gmail.com>
2017-01-08 23:26:36 +0000

    Added selected function that ads "active" class to selected options

--- src/app/components/quiz/questions-answers/questions-answers.component.ts
+++ src/app/components/quiz/questions-answers/questions-answers.component.ts
@@ -15,26 +15,44 @@ export class QuestionsAnswersComponent implements OnInit {
 
   private _quizQAUrl = "api/qaa.json";
   questionIndex: number = 1;
+  // selectedOptions: Array<string> = []
 
   _quiz: IQuiz[];
   errorMessage: string;
 
   constructor(private _http: Http) { }
 
+  // get the list of the questions and answers as an observable
   getQAAList(): Observable<IQuiz[]> {
     return this._http.get(this._quizQAUrl)
       .map(response => <IQuiz[]>response.json().quizData)
   }
-
+  // get one question and their answers by ID an an observable
   getQAA(id: number): Observable<IQuiz> {
     return this.getQAAList()
       .map(questions => questions.find(question => question.id === id));
   }
+
+  // selected options
+  selected(elem: any) {
+    elem.classList.toggle('active');
+    var options = document.getElementsByClassName('active');
+
+    for (var i = 0; i < options.length; i++) {
+      let selectedOptions = options[i].innerHTML;
+      console.log('selectedOptions: ' + selectedOptions)
+    }
+    console.log("number of selections: " + options.length);
+  }
+
+
+  // next question function
   nextQuestion() {
     if (this.questionIndex < 5) {
       this.questionIndex++;
     }
   }
+
   finishQuiz() {
 
   }
