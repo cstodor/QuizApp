8101a36221884456d961786b7c18c22a5d11ec29
Csaba Todor <todorcs@gmail.com>
2017-01-11 13:22:50 +0200

    Imported Quiz Service to Results Component

--- src/app/components/results/results.component.ts
+++ src/app/components/results/results.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+// Service 
+import { QuizService } from '../quiz/quiz.service';
+
 @Component({
   selector: 'app-results',
   templateUrl: './results.component.html',
-  styleUrls: ['./results.component.css']
+  styleUrls: ['./results.component.css'],
+  providers: [QuizService]
 })
 export class ResultsComponent implements OnInit {
 
-  constructor() { }
+  score: number;
+
+  constructor(private _quizService: QuizService) { }
 
   ngOnInit() {
+    this.score = this._quizService.getQuizScore();
   }
 
 }
