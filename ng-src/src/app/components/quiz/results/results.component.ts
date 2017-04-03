3de03caf546f0a5de9b5b0f6c557f7e87ae82ed9
cstodor <todorcs@gmail.com>
2017-04-04 08:31:11 +1200

    Updated ResultsComponent

--- ng-src/src/app/components/quiz/results/results.component.ts
+++ ng-src/src/app/components/quiz/results/results.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Output, EventEmitter, OnDestroy } from '@angular/core';
-
+import { Router } from '@angular/router';
 // Service
 import { QuizService } from '../quiz.service';
 
@@ -12,21 +12,53 @@ export class ResultsComponent implements OnInit {
 
   quizScore: number = null;
   @Output() scoreSend = new EventEmitter<number>();
+  name: String;
 
-  constructor(private _quizService: QuizService) { }
+  constructor(
+    private _quizService: QuizService,
+    private router: Router) { }
 
   ngOnInit() {
-    // Get Score from QuizService
+    // Get Current Score
     this.quizScore = this._quizService.score;
   }
 
+  registerScore() {
+
+    // result object
+    const result = {
+      name: this.name,
+      score: this.quizScore,
+      date: new Date()
+    }
+
+    // Name is required!
+    if (!this._quizService.validateName(result)) {
+      console.log('Please enter your name!');
+      return false;
+    }
+
+    // result object registration 
+    this._quizService.registerResult(result).subscribe(data => {
+      if (data.success) {
+        this.router.navigate(['/high-scores']);
+      }
+    },
+      err => {
+        console.log(err);
+        return false;
+      });
+  }
+
+
   // Send Score to Quiz Component (parent)
   sendScore() {
+    console.log("sendScore()")
     this.scoreSend.emit(this.quizScore);
   }
 
   ngOnDestroy() {
-    console.log('RESULTS DESTROYED, Score & Boolean Nulled.')
+    console.log('RESULTS ARE DESTROYED.')
     this._quizService.quizScore(null);
     this._quizService.quizDone(false);
   }
