63f7da091339a703a6f445002badc7f80a3728d3
cstodor <todorcs@gmail.com>
2017-04-12 04:43:06 +1200

    Adde Profile Routing

--- ng-src/src/app/app-routing.module.ts
+++ ng-src/src/app/app-routing.module.ts
@@ -2,14 +2,17 @@ import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
+// Components
 import { HomeComponent } from './components/home/home.component';
 import { QuizComponent } from './components/quiz/quiz.component';
 import { HighScoresComponent } from "./components/high-scores/high-scores.component";
+import { ProfileComponent } from "./components/profile/profile.component";
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'quiz', component: QuizComponent },
   { path: 'high-scores', component: HighScoresComponent }
+  { path: 'profile', component: ProfileComponent }
 ]
 
 @NgModule({
