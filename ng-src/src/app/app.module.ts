4e62983af84e01b35c89a76c98f866823563738f
cstodor <todorcs@gmail.com>
2017-04-12 04:42:44 +1200

    Created Profile Component

--- ng-src/src/app/app.module.ts
+++ ng-src/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { TimerComponent } from './components/quiz/timer/timer.component';
 import { QuestionsAnswersComponent } from './components/quiz/questions-answers/questions-answers.component';
 import { ResultsComponent } from './components/quiz/results/results.component';
 import { HighScoresComponent } from './components/high-scores/high-scores.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { HighScoresComponent } from './components/high-scores/high-scores.compon
     TimerComponent,
     QuestionsAnswersComponent,
     ResultsComponent,
-    HighScoresComponent
+    HighScoresComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
