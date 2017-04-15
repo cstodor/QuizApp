a528d7ed15479e99ec266aae5b13d471e5eed0e6
cstodor <todorcs@gmail.com>
2017-04-16 01:17:39 +1200

    Updated QuizService

--- ng-src/src/app/components/quiz/quiz.service.ts
+++ ng-src/src/app/components/quiz/quiz.service.ts
@@ -9,30 +9,21 @@ export class QuizService {
   score: number = null;
   time: number;
 
-  constructor(private http: Http) { }
+  constructor(public http: Http) { }
 
   // Get Quiz Questons and Answers
   getQuizData() {
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
-    return this.http.get('http://localhost:5000/api/quiz', { headers: headers }) // update url on deployment
+    return this.http.get('http://localhost:5000/api/quiz', { headers: headers })
       .map(res => res.json());
-  }
-  
-  // Validate Name
-  validateName(result) {
-    if (result.name === undefined || result.name === "") {
-      return false;
-    } else {
-      return true;
-    }
   }
 
   // Save Quiz Results
   registerResult(result) {
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
-    return this.http.post('http://localhost:5000/api/scores/save', result, { headers: headers }) // update url on deployment
+    return this.http.post('http://localhost:5000/api/scores/save', result, { headers: headers })
       .map(res => res.json());
   }
 
@@ -40,17 +31,20 @@ export class QuizService {
   getHighScores() {
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
-    return this.http.get('http://localhost:5000/api/scores/high-scores', { headers: headers }) // update url on deployment
+    return this.http.get('http://localhost:5000/api/scores/high-scores', { headers: headers })
       .map(res => res.json());
   }
 
   quizDone(data: boolean) {
     this.isQuizDone = data;
   }
+
   quizScore(data: number) {
     this.score = data;
   }
+
   timeLeft(data: number) {
     this.time = data;
   }
+  
 }
